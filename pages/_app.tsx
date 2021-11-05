c0800edf48397731402a9b16de3d1edbeabb6d37
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-05 14:34:09 +0400

    start dev by actions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import "react-toastify/dist/ReactToastify.css";
+import Actions from "actions/_app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
+      <Actions.Head>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           href="https://overreacted.io/favicon-32x32.png?v=8c19a894ebc3f54d282a8f2418cf5398"
           type="image/png"
         />
-      </Head>
+      </Actions.Head>
       <Component {...pageProps} />
     </>
   );
